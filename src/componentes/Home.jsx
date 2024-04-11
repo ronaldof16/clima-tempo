55733ba87fb9564902fe5aa70c3e3a805a117db4
ronaldof16 <ronaldof16@hotmail.com>
2024-04-11 14:13:44 -0300

    utilização da propriedade Object.keys para fazer uma condicional e exibir as informações na tela

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -3,34 +3,19 @@ import { useState } from 'react';
 import "./Home.css";
 
 const Home = () => {
-// 76174089709eb4883efb980c425965d4
 
     const [cidade, setCidade] = useState("");
     const [informacoes, setInformacoes] = useState({});
-    const [temperatura, setTemperatura] = useState(0);
-    const [tempo, setTtempo] = useState("nada");
 
     function handleChange(e) {
         setCidade(e.target.value);
     }
 
     async function buscaClima() {
-        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&lang=pt_br&appid=76174089709eb4883efb980c425965d4`)
-        .then(r => r.json())
-        .then((r) => {
-            setInformacoes(r)
-            setTemperatura(informacoes.main.temp)
-            setTtempo(informacoes.weather[0].description)});
-        console.log(informacoes)
-        
-        //Tentativa que não funcionou
-        /*await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&lang=pt_br&appid=76174089709eb4883efb980c425965d4`)
-        .then(r => r.json())
-        .then(r => setInformacoes(r))
-        console.log(informacoes)
-        await console.log(informacoes.main.tem)
-        console.log(informacoes.main.temp)*/
-        
+        const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&lang=pt_br&appid={ CHAVE_API }`);
+        const data = await resposta.json();
+        console.log(data);
+        setInformacoes(data)   
     }
 
   return (
@@ -40,11 +25,15 @@ const Home = () => {
             <input type="text" value={cidade} onChange={handleChange} placeholder='Digite a cidade'/>
             <button onClick={buscaClima}>Buscar</button>
         </div>
+
+        {Object.keys(informacoes).length > 0  && (
         <div>
             <h2>{informacoes.name}</h2>
-            <p>{parseInt(temperatura)}°C</p>
-            <p>{tempo}</p>
+            <p>{parseInt(informacoes.main.temp)}°C</p>
+            <p>{informacoes.weather[0].description}</p>
         </div>
+        )}
+        
     </div>
   )
 }
