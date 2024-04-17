647109b04b270c880e7b754f66aae2c4110ced33
ronaldof16 <ronaldof16@hotmail.com>
2024-04-17 17:53:19 -0300

    criação do arquivo config.js para criar a BASE_URL e deixar o código mais limpo e organizado

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+const APP_KEY = import.meta.env.VITE_KEY_OPEN_WATHER_MAP
+
+const fetchWeatherFromCity = async (cityName) => {
+    const resposta = await fetch(`${BASE_URL}?q=${cityName}&units=metric&lang=pt_br&appid=${APP_KEY}`);
+    return await resposta.json();
+}
+
+export {
+    fetchWeatherFromCity
+}
