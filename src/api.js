78835d8eae5a46a76048d70001538ef4fc01cf07
ronaldof16 <ronaldof16@hotmail.com>
2024-04-22 20:02:44 -0300

    lançando um erro para os casos em que o usuário digitar uma cidade que não existe

--- src/api.js
+++ src/api.js
@@ -3,7 +3,11 @@ const APP_KEY = import.meta.env.VITE_KEY_OPEN_WATHER_MAP
 
 const fetchWeatherFromCity = async (cityName) => {
     const resposta = await fetch(`${BASE_URL}?q=${cityName}&units=metric&lang=pt_br&appid=${APP_KEY}`);
-    return await resposta.json();
+    const resJson = await resposta.json();
+    if (resJson.cod === "404") {
+        throw Error("Cidade não encontrada, digite novamente!");
+    }
+    return resJson;
 }
 
 export {
